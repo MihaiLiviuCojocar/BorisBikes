3211641e4d3074bc2e6c0921b10bfc0ec778d92a
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-01-21 14:24:30 +0000

    modified Bike class and rspec file

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,20 @@
 class Bike
-	def broken?
+
+	# the initialize method is always called when you create a new class by typing Bike.new
+	def initialize
+		# all instance variables begin with "@"
+		# this must be an instance variable because we'll need it in another methods
+		@broken = false
 	end
+
+	def broken?
+		# instance variables are accesssible in all methods
+		@broken
+	end
+
+	def break
+		# any instance method can update them
+		@broken = true
+	end
+
 end
