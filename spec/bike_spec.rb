bf571f27a2178fd9cfc71f19f96e4c238881218c
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-01-21 13:48:50 +0000

    bike rspec file added

---
+++ spec/bike_spec.rb
@@ -0,0 +1,16 @@
+# link to the bike class
+require './lib/bike'
+
+# we're describing the functionalityvof o specific class, Bike
+describe Bike do 
+
+	# this is a specific feature (behaviour)
+	# that we expect to be present
+	it "should not be broken after we create it" do 
+		bike = Bike.new # initialise a new object of Bike class
+		# expect an instance of this class to have
+		# a method "broken?" that should return false
+		expect(bike).not_to be_broken
+	end
+	
+end
