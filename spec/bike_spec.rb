e4272bc03e04ee1c429873ed28ae05eed03ddedd
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-01-27 23:41:57 +0000

    handling a full station

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -32,6 +32,10 @@ end
 
 describe DockingStation do 
 
+	def fill_station(station)
+		20.times {station.dock(Bike.new)}
+	end
+
 	let(:bike)    { Bike.new }
 	let(:station) { DockingStation.new(:capacity => 20)}
 
@@ -56,6 +60,12 @@ describe DockingStation do
 		expect(station).not_to be_full
 		20.times {station.dock(Bike.new)}
 		expect(station).to be_full
+	end
+
+	it "should not accept a bike if it's full" do 
+		# 20.times {station.dock(Bike.new)}
+		fill_station(station)
+		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
 	end
 
 end
